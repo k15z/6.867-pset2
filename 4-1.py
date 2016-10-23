6ec9824ce62a756ee215882ea5ddd5f9f0e99067
Kevin Zhang <kevz@mit.edu>
2016-10-23 19:04:18 -0400

    MNIST linear models

--- 4-1.py
+++ 4-1.py
@@ -5,17 +5,40 @@ import matplotlib.pyplot as plt
 from quadSVM import QuadSVM, make_gaussian_kernel
 from sklearn.linear_model import LogisticRegression
 
-digit_4 = np.loadtxt('data/mnist_digit_4.csv')
-digit_9 = np.loadtxt('data/mnist_digit_9.csv')
+def load_data(pos, neg, normalize=False):
+    pos_x = [np.loadtxt('data/mnist_digit_' + i + '.csv') for i in pos]
+    neg_x = [np.loadtxt('data/mnist_digit_' + i + '.csv') for i in neg]
+    
+    pos_x_train = [arr[0:200,:] for arr in pos_x]
+    pos_x_val = [arr[200:350,:] for arr in pos_x]
+    pos_x_test = [arr[350:500,:] for arr in pos_x]
+    neg_x_train = [arr[0:200,:] for arr in neg_x]
+    neg_x_val = [arr[200:350,:] for arr in neg_x]
+    neg_x_test = [arr[350:500,:] for arr in neg_x]
+    
+    x_train = np.vstack(pos_x_train + neg_x_train)
+    y_train = np.hstack((
+        np.ones(200*len(pos_x_train)), -np.ones(200*len(neg_x_train))
+    ))
+    
+    x_val = np.vstack(pos_x_val + neg_x_val)
+    y_val = np.hstack((
+        np.ones(150*len(pos_x_val)), -np.ones(150*len(neg_x_val))
+    ))
+    
+    x_test = np.vstack(pos_x_test + neg_x_test)
+    y_test = np.hstack((
+        np.ones(150*len(pos_x_test)), -np.ones(150*len(neg_x_test))
+    ))
+    
+    if normalize:
+        x_train = x_train * 2.0 / 255.0 - 1.0
+        x_val = x_val * 2.0 / 255.0 - 1.0
+        x_test = x_test * 2.0 / 255.0 - 1.0
+    
+    return (x_train, y_train, x_val, y_val, x_test, y_test)
 
-x_train = np.vstack((digit_4[0:200,:], digit_9[0:200,:]))
-y_train = np.hstack((np.ones(200), -np.ones(200)))
-
-x_val = np.vstack((digit_4[200:350,:], digit_9[200:350,:]))
-y_val = np.hstack((np.ones(150), -np.ones(150)))
-
-x_test = np.vstack((digit_4[350:500,:], digit_9[350:500,:]))
-y_test = np.hstack((np.ones(150), -np.ones(150)))
+x_train, y_train, x_val, y_val, x_test, y_test = load_data("02468","13579")
 
 # sklearn.LR
 lr = LogisticRegression(C=1.0)
@@ -43,15 +66,16 @@ print("SVM Test", svm.score(x_test, y_test))
 print("SVM Val", svm.score(x_val, y_val))
 print("")
 
-
+"""
 result = svm.predict(x_test) * y_test > 0
 for i in range(len(result)):
     if not result[i]:
         image = x_test[i,:].reshape(28,28)
         plt.figure(1, figsize=(3, 3))
         if y_test[i] > 0:
-            plt.title("4")
+            plt.title("Positive")
         else:
-            plt.title("9")
+            plt.title("Negative")
         plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
         plt.show()
+"""
